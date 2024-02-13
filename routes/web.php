15d466d07fbcf0cf82132ba10b7e1e0d27adfaca
Darlis A. Amorim <darlisamorim@gmail.com>
2024-02-13 16:33:44 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
+});
+
+Route::prefix('produtos')->group(function () { 
+    Route::get('/', [ProductController::class,'index'])->name('produto.index');
 });
